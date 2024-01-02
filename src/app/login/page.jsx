b1ebf248a62093008a74378a8ad327cb8e442cd7
unknown <tarun.tiwari@velocis.co.in>
2024-01-02 17:18:34 +0530

    login and signup validation

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,66 +1,71 @@
 "use client"
 import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import './Login.css';
 import Link from 'next/link';
-import { Button } from '@mui/material';
+import { Button, TextField, CircularProgress } from '@mui/material';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const handleLogin = () => {
-    if (username.trim() === '' || password.trim() === '') {
-      setError('Username and password cannot be blank');
-    } else {
-      setError('');
-    
-    }
-  };
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+        username: Yup.string().min(5, 'Username must be at least 5 characters').required('Username is required'),
+        password: Yup.string().min(5, 'Password must be at least 5 characters').required('Password is required'),
+    }),
+    onSubmit: async (values) => {
+      setLoading(true);
+      setTimeout(async () => {
+
+        setLoading(false);
+
+        window.location.href = '/home';
+      }, 2000);
+    },
+  });
 
   return (
     <div className='home-image'>
-      <div className='login-container '>
+      <div className='login-container'>
         <div className='login-box'>
           <h1 className='heading'>VIRA</h1>
-          <div className='form-group'>
-            <label htmlFor='username'>Username:</label>
-            <input
-              type='text'
-              id='username'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Password:</label>
-            <input
-              type='password'
-              id='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className='signup'>
-            <Link href="signUp"> <h6>Sign Up</h6>  </Link>
-            
-          </div>
-          {error && <div className='error-message'>{error}</div>}
-          
-          {!error ? (
-    <Button  variant="outlined"  className="w-100 py-2" onClick={handleLogin}>
-      Login
-    </Button>
-) : (
-  <Link href="/home">
-
-  <Button className="w-100 py-2" variant="outlined" >
-    Login
-  </Button>
-  </Link>
-
-)}
-
+          <form onSubmit={formik.handleSubmit}>
+            <div className='form-group'>
+              <label htmlFor='username'>Username:</label>
+              <TextField
+                fullWidth
+                id='username'
+                type='text'
+                {...formik.getFieldProps('username')}
+                error={formik.touched.username && Boolean(formik.errors.username)}
+                helperText={formik.touched.username && formik.errors.username}
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Password:</label>
+              <TextField
+                fullWidth
+                id='password'
+                type='password'
+                {...formik.getFieldProps('password')}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+              />
+            </div>
+            <div className='signup'>
+              <Link href='/signUp'>
+                <h6>Sign Up</h6>
+              </Link>
+            </div>
+            <Button type='submit' variant='outlined' className='w-100 py-2'>
+              {loading ? <CircularProgress size={24} /> : 'Login'}
+            </Button>
+          </form>
         </div>
       </div>
     </div>
